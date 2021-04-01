3a7d5c41aac113a5433ca25006cc6d7dadc2a953
R-ovo-stack <609602477@qq.com>
2021-04-01 15:27:39 +0800

    Site updated: 2021-04-01 15:27:26

---
+++ js/FunnyTitle.js
@@ -0,0 +1,16 @@
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+         document.title = '赶紧回来呀！';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+         document.title = '哈哈，真听话！' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
